5e488061283a42e2d7828a74b3e69623bae2bf84
manish <aryam7842@gmail.com>
2025-02-08 01:14:56 +0530

    Initial commit with cleaned project files

---
+++ ChaiaurDjango/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# def home(request):
+#     # return HttpResponse("Hello, world! \n You are in home page of Chai aur Code")
+#     return render(request, 'manish.html')
+
+
